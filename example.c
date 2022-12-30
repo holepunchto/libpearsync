2fbec8f157766347abfe2bf59e80d8bc0c0b8290
Mathias Buus <mathiasbuus@gmail.com>
2022-12-30 12:00:47 +0100

    better example

--- example.c
+++ example.c
@@ -2,24 +2,25 @@
 #include <uv.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void
 on_wakeup_thread (pearsync_port_t *a) {
-  printf("wakeup thread...\n");
+  printf("woke up thread...\n");
 
   pearsync_msg_t m;
   while (pearsync_recv(a, &m)) {
-    printf("thread recv: %s\n", m.data);
+    printf("thread recv: %s\n", (char *) m.data);
   }
 }
 
 void
-on_wakeup_main  (pearsync_port_t *a) {
-  printf("wakeup main... %i\n", a->is_main ? 1 : 0);
+on_wakeup_uv  (pearsync_port_t *a) {
+  printf("woke up main...\n");
 
   pearsync_msg_t m;
   while (pearsync_recv(a, &m)) {
-    printf("main recv: %s\n", m.data);
+    printf("uv recv: %s\n", (char *) m.data);
   }
 }
 
@@ -27,8 +28,34 @@ void
 run_thread (void *data) {
   pearsync_t *a = (pearsync_t *) data;
 
-  printf("i am a thread\n");
-  pearsync_open_thread(a, pearsync_wakeup, on_wakeup_thread);
+  printf("running on a thread...\n");
+
+  pearsync_port_t *p = pearsync_open_thread(a, pearsync_wakeup, on_wakeup_thread);
+
+  for (int i = 0; i < 32; i++) {
+    pearsync_msg_t m = {
+      .len = 32,
+      .data = malloc(32)
+    };
+
+    m.len = sprintf(m.data, "hello uv #%i", i) + 1;
+
+    pearsync_send(p, &m);
+  }
+
+  printf("thread is sleeping 2s and then sending 4 more\n");
+  sleep(2);
+
+  for (int i = 0; i < 4; i++) {
+    pearsync_msg_t m = {
+      .len = 32,
+      .data = malloc(32)
+    };
+
+    m.len = sprintf(m.data, "hello uv #%i", 32 + i) + 1;
+
+    pearsync_send(p, &m);
+  }
 }
 
 int
@@ -36,17 +63,20 @@ main () {
   pearsync_t a;
   pearsync_init(&a);
 
-  pearsync_port_t *p = pearsync_open_uv(&a, uv_default_loop(), on_wakeup_main);
+  pearsync_port_t *p = pearsync_open_uv(&a, uv_default_loop(), on_wakeup_uv);
 
-  pearsync_msg_t m = {
-    .len = 13,
-    .data = malloc(13)
-  };
+  // uncomment to send to thread
 
-  memcpy(m.data, "hello thread", 13);
+  // for (int i = 0; i < 32; i++) {
+  //   pearsync_msg_t m = {
+  //     .len = 32,
+  //     .data = malloc(32)
+  //   };
 
-  pearsync_send(p, &m);
-  pearsync_send(p, &m);
+  //   m.len = sprintf(m.data, "hello thread #%i", i) + 1;
+
+  //   pearsync_send(p, &m);
+  // }
 
   uv_thread_t id;
   uv_thread_create(&id, run_thread, &a);
